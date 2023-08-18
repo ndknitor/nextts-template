cc2f69059ac6d606abf7715bf888d5407a4616c0
kn <kn@debian-BULLSEYE-live-builder-AMD64>
2023-08-18 16:57:19 +0700

    toFormData

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import ThemeTextInput from '@/components/ThemeTextInput/ThemeTextInput'
 import SignInRequest from '@/objects/requests/SignInRequest'
-import React from 'react'
+import React, { useState } from 'react'
 import ThemeButton from '@/components/ThemeButton/ThemeButton';
 import { Fade, Stack } from '@mui/material';
 import ValidationWrap from '@/components/ValidationWrap/ValidationWrap';
@@ -12,10 +12,19 @@ import Link from 'next/link';
 import FadeInUp from '@/components/FadeInUp/FadeInUp';
 import FadeInDown from '@/components/FadeInDown/FadeInDown';
 import FadeZoomIn from '@/components/FadeZoomIn/FadeZoomIn';
+import appxios from '@/components/AxiosInterceptor';
+import useAsyncEffect from 'use-async-effect';
+import moment from 'moment';
 
 function page() {
+    const [date, setDate] = useState<Date>();
+    useAsyncEffect(async () => {
+        const response = await appxios.get<{ date: Date }>("http://13.212.116.185:5000/main");
+        setDate(response.data.date);
+    }, []);
     return (
         <FadeInDown>
+            <h1>{date && moment(date).format("YYYY:MM:DD HH:mm:ss")}</h1>
             <Stack padding={5}>
                 <Link href="/public/about">About</Link>
                 <ThemeButton onClick={async () => {
