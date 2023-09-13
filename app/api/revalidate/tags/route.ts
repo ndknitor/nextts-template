775045b67ac87af16205b41451c441ef5efc63f1
kn <nguyentuanpkn@gmail.com>
2023-09-13 09:08:51 +0700

    add revalidate tag

---
+++ app/api/revalidate/tags/route.ts
@@ -0,0 +1,27 @@
+import { HttpStatusCode } from "axios";
+import { revalidateTag } from "next/cache";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+    const token = req.headers.get("Revalidate-Token");
+    const body = { message: 'Invalid token', revalidated: false };
+    if (token !== process.env.REVALIDATE_TOKEN) {
+        return NextResponse.json(body, { status: HttpStatusCode.Forbidden });
+    }
+    const tags = (await req.json()) as string[];
+    if (!tags) {
+        body.message = "Tags is required";
+        return NextResponse.json(body, { status: HttpStatusCode.BadRequest });
+    }
+    try {
+        tags.map(tag => {
+            revalidateTag(tag);
+        });
+        body.revalidated = true;
+        body.message = "Revalidate successfully";
+        return NextResponse.json(body, { status: HttpStatusCode.Ok });
+    } catch (err) {
+        body.message = "Server error";
+        return NextResponse.json(body, { status: HttpStatusCode.InternalServerError });
+    }
+}
