2ed14567abaea7f06b6f4ccdc2555235a727475b
kn <nguyentuanpkn@gmail.com>
2023-09-14 12:32:17 +0700

    Service

--- services/UserService.ts
+++ services/UserService.ts
@@ -4,7 +4,7 @@ import { PagingResponse } from "@/objects/responses/PagingResponse";
 import { Dispatch, SetStateAction, use } from "react";
 import Service from "./Service";
 import SingleResponse from "@/objects/responses/SingleResponse";
-const tag = "User";
+const tags = ["User"];
 const revalidate = 60;
 const context = "/api/user";
 export default class UserService extends Service {
@@ -19,18 +19,18 @@ export default class UserService extends Service {
         }
         else {
             this.setLoading(true);
-            const result = await fetcher.fetch(context, { next: { revalidate: revalidate, tags: [tag] } });
+            const result = await fetcher.fetch(context, { next: { revalidate: revalidate, tags : tags } });
             this.setLoading(false);
             return (await result.json()) as PagingResponse<User>;
         }
     }
     async insert(user: User) {
-        return await appxios.post<SingleResponse<User>>(context, user, { loadAction: { setLoading: this.setLoading }});
+        return await appxios.post<SingleResponse<User>>(context, user, { loadAction: { setLoading: this.setLoading } });
     }
     async update(user: User) {
-        return await appxios.post<SingleResponse<User>>(context, user, { loadAction: { setLoading: this.setLoading }});
+        return await appxios.post<SingleResponse<User>>(context, user, { loadAction: { setLoading: this.setLoading } });
     }
-    async delete(userId : number) {
-        return await appxios.delete<SingleResponse<User>>(context, { loadAction: { setLoading: this.setLoading }});
+    async delete(userId: number) {
+        return await appxios.delete<SingleResponse<User>>(context, { loadAction: { setLoading: this.setLoading } });
     }
 }
