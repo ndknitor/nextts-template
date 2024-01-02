bfd2fb2945b42299c8125627abf4a77972bdfc26
kn <nguyentuanpkn@gmail.com>
2024-01-02 12:40:23 +0700

    remove unused function

--- utils/fetcker.ts
+++ utils/fetcker.ts
@@ -3,7 +3,7 @@ import { toast } from "react-toastify";
 
 const fetcker = createFetcker({
     baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL,
-    requestTimeOut: parseInt(process.env.NEXT_PUBLIC_REQUEST_TIMEOUT.toString()),
+    requestTimeOut: parseInt(process.env.NEXT_PUBLIC_REQUEST_TIMEOUT),
     onError: (error, isClient) => {
         if (isClient) {
             let message = "";
